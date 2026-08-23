8123753b923d5012ee6ea90c4aeeb055715137d6
Bhavethra Baabu <bhavethra2401@gmail.com>
2026-08-23 18:09:59 -0400

    Time: 15 ms (40.86%) | Memory: 23.8 MB (26.04%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict = defaultdict(list)
+        for s in strs:
+            count = [0]*26
+            for c in s:
+                count[ord(c)-97]+=1
+            key = tuple(count)
+            dict[key].append(s)
+        return list(dict.values())    
